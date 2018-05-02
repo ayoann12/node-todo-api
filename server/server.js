f6424d25d732656032da4df91a217d415c0ef8ac
unknown <ayoann12@yahoo.fr>
2018-05-02 11:20:17 +0100

    **NEW**: implementation of searching by ID with *get* method and tests

--- server/server.js
+++ server/server.js
@@ -1,16 +1,20 @@
+const {ObjectId} = require("mongodb");
 const express = require("express");
 const bodyParser = require("body-parser");
 
 const {mongoose} = require("./db/mongoose");
 const {Todo} = require("./models/todo");
-const {User} = require("./models/user");
 
+// In case i test it somewhere than local - Heroku for instance - i set 'port' variable to the 'env' default port
 const port = process.env.PORT || 3000;
+
 const app = express();
 
 app.use(bodyParser.json());
 
 /* eslint-disable */
+
+// Create one todo and save to the database
 app.post("/todos", (req, res) => {
 	const todo = new Todo({
 		text: req.body.text
@@ -23,9 +27,26 @@ app.post("/todos", (req, res) => {
 	});
 });
 
+// List all todos in the database
 app.get("/todos", (req, res) => {
 	Todo.find({}).then((todos) => {
 		res.send({todos});
+	}).catch((err) => {
+		res.status(400).send(err);
+	});
+});
+
+// Search for one particular todo by id
+app.get("/todos/:id", (req, res) => {
+	const id = req.params.id;
+	if (!ObjectId.isValid(id)) {
+		return res.status(404).send(`ID ${id} is invalid`);
+	}
+	Todo.findById(id).then((todo) => {
+		if (!todo) {
+			return res.status(404).send(`No todo found with id ${id}`);
+		}
+		res.send(todo);
 	}).catch((err) => {
 		res.status(400).send(err);
 	});
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,13 +4,16 @@
 
 const expect = require("expect");
 const request = require("supertest");
+const {ObjectId} = require("mongodb");
 
 const {app} = require("./../server");
 const {Todo} = require("./../models/todo");
 
 const todos = [{
+	_id: new ObjectId,
 	text: "First todo"
 }, {
+	_id: new ObjectId,
 	text: "Second todo"
 }];
 
@@ -74,6 +77,58 @@ describe("Test of GET /todos", () => {
 			.expect(200)
 			.expect((res) => {
 				expect(res.body.todos.length).toBe(2);
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+			});
+		done();
+	});
+});
+
+describe("Test of GET /todos/:id", () => {
+	it("should return the todo which ID was indicated", (done) => {
+		request(app)
+			.get(`/todos/${todos[0]._id}`)
+			.send()
+			.expect(200)
+			.expect((res) => {
+				expect(res.body.text).toBe(todos[0].text);
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+			});
+		done();
+	});
+
+	it("should return 404 for bad ID", (done) => {
+		const id = 123;
+		request(app)
+			.get(`/todos/${id}`)
+			.send()
+			.expect(404)
+			.expect((res) => {
+				expect(res.text).toBe(`ID ${id} is invalid`);
+			})
+			.end((err, res) => {
+				if (err) {
+					return done(err);
+				}
+			});
+		done();
+	});
+
+	it("should return 404 for inexistant ID", (done) => {
+		const id = "000000000000000000000000";
+		request(app)
+			.get(`/todos/${id}`)
+			.send()
+			.expect(404)
+			.expect((res) => {
+				expect(res.text).toBe(`No todo found with id ${id}`);
 			})
 			.end((err, res) => {
 				if (err) {
