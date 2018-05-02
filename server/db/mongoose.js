a1b9feb98d5beba7afcf209a93cd672d3c28681b
unknown <ayoann12@yahoo.fr>
2018-05-02 12:09:35 +0100

    **NEW** : Heroku start script on package.json, specification of node engine to use and configuration of mLab db to use with Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/TodoApp");
+
+// For using Mongo db on local env or mLab db on remote like Heroku
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost:27017/TodoApp");
 
 module.exports = {mongoose};
